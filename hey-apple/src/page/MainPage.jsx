157700be0f69e753821d1631f19fb066aac96a74
lee juyoung <88328887+phnml1@users.noreply.github.com>
2023-01-11 03:02:50 +0900

    feat: 클릭시 로고사라지고 사과 가운데로 이동

--- hey-apple/src/page/MainPage.jsx
+++ hey-apple/src/page/MainPage.jsx
@@ -3,24 +3,25 @@ import './scss/mainpage.scss';
 import { useCallback, useEffect, useState } from 'react';
 import logoImg from './image/icon4.png';
 import logoName from './image/name3.png';
-
+import classNames from 'classnames';
 function MainPage() {
   const [intro, setintro] = useState(true);
+  const [logotrans,setlogotrans] = useState("");
   const dis = "nodisplay";
-    
+  useEffect(()=>{if(intro==false){setlogotrans("logotrans")}},[intro]);
   return (
 
     <div id = "maincontainer" onMouseDown={(e) => {
       e.preventDefault()}
     } onClick = {()=>{setintro(false)}}> 
-    {intro===false?
-    <IntroLogo  dis = {dis}/>:<IntroLogo/>}
+    {intro?
+    <IntroLogo/>:null}
 
-    <div id = "intrologoimage" className='fade-in-box'/>
+    <div id = "intrologoimage" className={classNames('fade-in-box', {logotrans})}/>
     </div>
   );
 }
-function IntroLogo({dis}) {
+function IntroLogo() {
   const [intrologoName,setintrologoName] = useState(false)
   const [introdes, setintrodes] = useState(false);
   const [putmouse, setputmouse] = useState(false);
@@ -40,7 +41,7 @@ function IntroLogo({dis}) {
         clearTimeout(logo); //useEffect동작하기전에 특정코드실행(기존데이터요청충돌방지),unmount시에실행
       };
     }, []); //count변할때만실행[count], [] mount시에만실행
-  return(<div id = "intrologo" className = {dis}>
+  return(<div id = "intrologo">
   {intrologoName===true?<div id = "intrologoname" className='smoothAppearX'/>: null}
   {introdes===true?<div id = "introdes" className='fade-in-box'>comfortable food calculator</div>: null}
    <div id = "introhov" >         ↑   Click Here to use                  </div>
