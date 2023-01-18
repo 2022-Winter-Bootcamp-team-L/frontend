57489463c6c6162a7d839d91cc6ba64dd778ba86
Joohee.K <wnglgl22@tukorea.ac.kr>
2023-01-18 16:41:26 +0900

    feat: header route

--- hey-apple/src/components/Header.jsx
+++ hey-apple/src/components/Header.jsx
@@ -4,14 +4,18 @@ function Header() {
   return (
 <div id="header">
   <div id = "header_contents">
-  <div id = "header_home">
-    Home
+  <div id = "header_home" >
+    <a href="http://localhost:3000" style={{ textDecoration: 'none', color: '#000000', fontFamily: 'Quicksand', fontWeight: '400' }}>
+      Home
+    </a>
   </div>
-  <div id = "header_aboutus">
+  <div id = "header_aboutus" style={{ fontFamily: 'Quicksand', fontWeight: '500' }}>
     about us
   </div>
   <div id = "header_products">
-    products
+    <a href="http://localhost:3000/products" style={{ textDecoration: 'none', color: '#000000', fontFamily: 'Quicksand', fontWeight: '600' }}>
+      products
+    </a>
   </div>
   </div>
 </div>
