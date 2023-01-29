1edcf1cdc368a00e208e248907e4a38db104521c
Joohee.K <wnglgl22@tukorea.ac.kr>
2023-01-29 23:28:52 +0900

    feat: add email form to ResultsBox

--- hey-apple/src/components/ResultsBox.jsx
+++ hey-apple/src/components/ResultsBox.jsx
@@ -1,62 +1,54 @@
 import React from "react";
-import '../scss/Receipt.scss'
+import "../scss/Receipt.scss"
 import Divider from "./Divider";
+import EmailForm from "./EmailForm";
 
-export default function ResultsBox({keys,values,total}) {
-    console.log(keys)
-    console.log(values)
-    return (
-        <box id="receipt">
-            
-            <div id="receipt_title">
-                receipt
+export default function ResultsBox({id, keys, values, total }) {
+  console.log(id);
+  console.log(values);
+  return (
+    <box id="receipt">
+      <div id="receipt_title">receipt</div>
+      <div
+        style={{
+          marginTop: "5%",
+        }}
+      >
+        <Divider />
+      </div>
+      <div id="receipt_header_layout">
+        <div id="receipt_header">Item Details</div>
+        <div id="receipt_header">#</div>
+        <div id="receipt_header">Price</div>
+      </div>
+      <Divider />
+      <div>
+        <Divider />
+      </div>
+      <div id="fruitscontainer">
+        {values.map(function (a, i) {
+          return (
+            <div id="fruitinfocontainer">
+              <div id="receiptfruitnames">{keys[i]}</div>
+              <div id="receiptfruitcount">{values[i].count}</div>
+              <div id="receiptfruitprice">
+                {values[i].price * values[i].count}
+              </div>
             </div>
-            <div style={{
-                marginTop: '5%'
-            }}>
-                <Divider/>
-            </div>
-            <div id="receipt_header_layout">
-                <div id="receipt_header">
-                    Item Details
-                </div>
-                <div id="receipt_header">
-                    #
-                </div>
-                <div id="receipt_header">
-                    Price
-                </div>
-            </div>
-            <Divider/>
-            <div>
-                <Divider/>
-            </div>
-            <div id = "fruitscontainer">
-                {values.map(function(a,i){
-                return(
-                <div id = "fruitinfocontainer">
-                    <div id = "receiptfruitnames">{keys[i]}</div>
-                    <div id = "receiptfruitcount">{values[i].count}</div>
-                    <div id = "receiptfruitprice">{values[i].price*values[i].count}</div>
-                </div>);})}
-            </div>
-            <div id = "receipttotal">
-                    <b>total:</b> {total}
-            </div>
-            {/* <Divider style={{ 
-                marginTop:'5%',
-                marginBottom: '5%'
-            }} /> */}
-            <button
-                id="payment_button"
-                onClick={() => {
-                    alert('Your payment was successful.');
-                }}
-            >
-                PAY
-            </button>
-            
-          
-        </box>
-    );
+          );
+        })}
+      </div>
+      <div id="receipttotal">
+        <b>total:</b> {total}
+      </div>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        textAlign: 'center',
+        marginTop: '18.8vw'
+      }}>
+        <EmailForm id = {id}/>
+      </div>
+    </box>
+  );
 }
