ea04831d12e7726954ecd37a3c6be74a8ae45be1
lee juyoung <88328887+phnml1@users.noreply.github.com>
2023-02-01 18:17:37 +0900

    feat: 이메일체크 애니메이션추가

--- hey-apple/src/components/EmailCheck.jsx
+++ hey-apple/src/components/EmailCheck.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import '../scss/Result.scss'
-
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
+import { useState } from "react";
 export default function EmailCheck() {
+  let navigate = useNavigate();
+  const [homelink,sethomelink] = useState(false)
+  useEffect(()=>{let hometimeout= setTimeout(()=>{sethomelink(true);},1000); return()=>{clearTimeout(hometimeout)}},[])
     return(
         
-         <div id = "emailsuccess">
-          <div id = "emailcheckimage">
+         <div id = "emailsuccess" className = "fade-in-box">
+          <div id = "emailcheckimage" >
           <img src={process.env.PUBLIC_URL + "/image/emailsuccess.png"} width="100%"  alt="이미지안나옴" />
           </div>
           <div id = "emailratio"></div>
@@ -13,8 +18,11 @@ export default function EmailCheck() {
 
           </div>
           <div id = "emailtext">
-            이메일이 전송되었습니다.
+            email's sended successfully
           </div>
+          {(homelink)?(<div id = "emailhomelink"  onClick = {()=>navigate('/')}>
+            go to home
+          </div>):null}
          </div>  
     );
 }
